7d26f5da2286487696fdfb3aea0b095939de00d7
EvanM <kixew34@gmail.com>
2025-02-26 07:26:46 -0500

    Finaly it works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 use rand::Rng;
 
+fn generate_password(length: u32, characters_map: &str, rng: &mut impl rand::Rng) -> String {
+    let characters_map_length = characters_map.len() as u32;
+    let mut password = String::new();
+    for _ in 0..length {
+        let index = rng.random_range(0..characters_map_length);
+        let character = characters_map.chars().nth(index as usize).unwrap();
+        password.push(character);
+    }
+    password
+}
+
 fn main() {
+    println!("Hello, please give me the characters map you will use:");
+    let mut characters_map = String::new();
     let mut rng = rand::rng();
-    let secret_number = rng.random_range(1..10);
-    println!("Hello, world! The secret number is: {}", secret_number);
+    std::io::stdin().read_line(&mut characters_map).expect("Failed to read line");
+    let characters_map = characters_map.trim();
+    println!("You will use the following characters map: {}", characters_map);
+    println!("Please give me the length of the password you want:");
+    let mut length = String::new();
+    std::io::stdin().read_line(&mut length).expect("Failed to read line");
+    let length: u32 = length.trim().parse().expect("Please type a number!");
+    println!("You want a password of length: {}", length);
+    let password = generate_password(length, characters_map, &mut rng);
+    println!("Your password is: {}", password);
 }
