e2f955a711719323f5ef49f3161212b135a8c732
EvanM <kixew34@gmail.com>
2025-02-26 07:14:55 -0500

    Testing: rng.random_range()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::rng();
+    let secret_number = rng.random_range(1..10);
+    println!("Hello, world! The secret number is: {}", secret_number);
 }
